e0f8e47e78746f2f2fe7ddc3cc9f2c6c439f3b10
futbolero06 <oscarardila@colegioguanenta.edu.co>
2023-03-22 07:16:13 -0500

    se modifico el diagrama y programa

--- piedra_papel_tigera.py
+++ piedra_papel_tigera.py
@@ -15,31 +15,42 @@ maquina = random.choice(["piedra","papel","tigera"])
 
 if (maquina=="piedra"):
     if (jugador==1):
+        jug="piedra"
         rta="empate"
     elif (jugador==2):
+        jug="papel"
         rta= "ganaste"
     else:
+        jug="tijeras"
         rta="perdiste"
 
 if (maquina=="papel"): 
     if (jugador==2):
+        jug="papel"
         rta="empate"
     elif (jugador==3):
+        jug="tijeras"
         rta= "ganaste"
     else:
+        jug="piedra"
         rta="perdiste"
 
 if (maquina=="tigera"):
     if (jugador==3): 
+        jug="tijeras"
         rta="empate"
     elif (jugador==1):
+        jug="piedra"
         rta= "ganaste"
     else:
+        jug="papel"
         rta="perdiste"
 
 
 #output
             
-print("la maquina saco: " + str(maquina) + " Y " + str(rta ))
+print("la maquina saco: " + str(maquina) )
+print("usted escogió: " + str(jug))
+print(rta)
 
 
